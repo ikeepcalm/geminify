03cd526aa6868137c5bce96355d0f2fe5f1face6
ikeepcalm <horokhbohdandmytrovich@gmail.com>
2025-07-24 09:52:11 +0300

    Update deployment configuration and Dockerfile for Geminify application

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM openjdk:21-jdk-slim
 
 WORKDIR /app
 
-COPY build/libs/geminify-1.0.0.jar app.jar
+COPY build/libs/Geminify-0.0.1-SNAPSHOT.jar app.jar
 
 EXPOSE 8080
 
--- src/test/java/dev/ua/ikeepcalm/Geminify/GeminifyApplicationTests.java
+++ src/test/java/dev/ua/ikeepcalm/Geminify/GeminifyApplicationTests.java
@@ -1,13 +1,8 @@
 package dev.ua.ikeepcalm.Geminify;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GeminifyApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
